0d0d47aa181f513bea25363e0dae8d2e98dd67af
s18725 <61834255+s18725@users.noreply.github.com>
2020-04-19 15:44:54 +0200

    Update StudentsController.cs

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -27,10 +27,21 @@ namespace Cw5.Controllers
             return response;
         }
 
-        [HttpGet("{indexNumber}")]
-        public Student GetStudent(string id){
-            var response = _db.GetStudent(id);
-            return response;
+         [HttpGet("secured/{id}")]
+        public IActionResult GetStudent(string id)
+        {
+            if (_db.GetStudent(id) != null)
+            {
+                return Ok(_db.GetStudent(id));
+            }
+
+            return NotFound("Nie znaleziono studenta");
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetStudent2(string id)
+        {
+             return Ok(_db.GetStudent(id));
+
         }
     }
 }
